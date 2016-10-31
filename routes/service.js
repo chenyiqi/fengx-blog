a2ae0a4e64f6828ec92a8ee2c8ae29aad8ec1c4c
王颖 <564951229@qq.com>
2016-10-31 20:02:50 +0800

    feat(origin) : 上传初始代码

---
+++ routes/service.js
@@ -0,0 +1,30 @@
+/**
+ * Created by wangy on 2016/10/31.
+ */
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host:"qdm216841450.my3w.com",
+    user:"qdm216841450",
+    password:"Wy564951229",
+    port:"3306",
+    database:"qdm216841450_db",
+    insecureAuth:"true"
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    connection.query("select count(*) from wp_users",function(error,rs,status){
+        if(error){
+            res.write("-1");
+            res.end();
+        }else{
+            res.write(""+rs[0]["count(*)"]);
+            res.end();
+        }
+    });
+});
+
+module.exports = router;
